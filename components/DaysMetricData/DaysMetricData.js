1645b1a5f6d1f756a198dbd94d096486dc650059
Matt Harrison <MattHarrison92@gmail.com>
2022-03-18 09:36:02 -0500

    if no values entered in custom form, display null

--- components/DaysMetricData/DaysMetricData.js
+++ components/DaysMetricData/DaysMetricData.js
@@ -13,8 +13,9 @@ export default function daysMetricData({metrics, units}) {
             <div>
                 <h3><MetricsDate dateString={metrics.date} /></h3>
                 {metrics.metrics.map((metric, index) => {
+                    console.log(Object.values(metric))
                     return <div>
-                        <p>{Object.keys(metric)}: {Object.values(metric)} {(units.metrics[index].units) ? units.metrics[index].units : '' }</p>
+                        <p>{Object.keys(metric)}: {Object.values(metric)} {(Object.values(metric) == '') ? 'Null' : ((units.metrics[index].units) ? units.metrics[index].units : '')}</p>
                     </div>
                 })}
             </div>
